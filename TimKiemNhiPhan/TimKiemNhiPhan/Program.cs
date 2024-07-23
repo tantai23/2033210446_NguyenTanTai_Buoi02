786a0db4b0f2ea3a3ed55cdac82ea317e622b800
tantai23 <tantainguyen002323@gmail.com>
2024-07-23 10:58:14 +0700

    init project

---
+++ TimKiemNhiPhan/TimKiemNhiPhan/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace timkiemnhiphan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sortedArray = { 2, 5, 7, 10, 15, 18, 21, 25, 30, 35 };
+            int target = 18;
+
+            int resultIndex = BinarySearch(sortedArray, target);
+
+            if (resultIndex != -1)
+            {
+                Console.WriteLine($"phan tu {target} duoc tim thay tai chi so {resultIndex}.");
+            }
+            else
+            {
+                Console.WriteLine($"phan tu {target} khong co trong mang.");
+            }
+        }
+
+        static int BinarySearch(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int middle = left + (right - left) / 2;
+
+                // So sánh phần tử giữa với giá trị tìm kiếm
+                if (array[middle] == target)
+                {
+                    return middle;
+                }
+
+                // Nếu phần tử tìm kiếm lớn hơn phần tử giữa, tìm ở nửa bên phải
+                if (array[middle] < target)
+                {
+                    left = middle + 1;
+                }
+                // Nếu phần tử tìm kiếm nhỏ hơn phần tử giữa, tìm ở nửa bên trái
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+
+            // Nếu không tìm thấy phần tử
+            return -1;
+        }
+    }
+}
---
+++ nhan2so/nhan2so/nhan2so/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;// khai báo để sử dụng biết bigintger
+namespace Nhân_2_số
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger num1, num2, product;
+
+            Console.Write("nhap so nguyen dau tien: ");
+            num1 = BigInteger.Parse(Console.ReadLine());
+
+            Console.Write("nhap so nguyen thu 2: ");
+            num2 = BigInteger.Parse(Console.ReadLine());
+
+            product = num1 * num2;
+
+            Console.WriteLine("ket qua cua " + num1 + " va " + num2 + " la: " + product);
+        }
+    }
+}
